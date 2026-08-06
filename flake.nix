b610e18e2f4f50ce85a0652aadf2e08ed30a0775
Kyamel <lcamelo200@gmail.com>
2026-08-06 20:20:19 -0300

    feat: nix dev env

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "darktile - GPU rendered terminal emulator";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            go
+            pkg-config
+            git
+            gopls
+            delve
+            goreleaser
+          ];
+
+          buildInputs = with pkgs; [
+            libGL
+            libx11
+            libxcursor
+            libxi
+            libxinerama
+            libxrandr
+            libxxf86vm
+          ];
+        };
+      });
+    };
+}
